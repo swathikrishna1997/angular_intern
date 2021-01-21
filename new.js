7c377c2e58e3669d895a857d9d1791b4c103ccc3
swathikrishna1997 <58287310+swathikrishna1997@users.noreply.github.com>
2021-01-21 15:09:53 +0530

    initial commit

---
+++ new.js
@@ -0,0 +1,39 @@
+var num=45;
+var string=String(num);
+console.log(typeof(num));
+var b=78;
+var c=string+b;
+if(typeof(c)==Number)
+{
+console.log(c)
+}
+else
+console.log(typeof(c))
+//const d=null ?? 'null value';
+//const h=8 ?? 'not null';
+var g=8;
+do{
+    console.log(g);
+    g--;
+}
+while(g>=0){
+    
+}
+console.log(g)
+for(i=0;i<10;i++){
+    for(j=0;j<10;j++)
+    {
+        console.log(i+j);
+    }
+}
+var value="cat";
+switch(value){
+    case "cat":
+        console.log("cat");
+        break;
+        case "dog":
+            console.log("dog");
+            break;
+            default:
+                console.log("none")
+}
